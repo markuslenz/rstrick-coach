26fb85b2f6f7fa767d8dae3d3f7d785d963d7fb9
Markus Lenz <git@lenzma.de>
2026-03-03 23:07:02 +0100

    Removed laravel boost & adjusted vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    optimizeDeps: {
+        exclude: ['lightningcss'],
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
